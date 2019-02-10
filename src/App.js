bf27434b18efcbd169d0b71860d0b79de47be019
Apoorv <apoorvm8@gmail.com>
2019-02-11 02:49:16 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import Contacts from './components/contacts/Contacts';
+import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
+import AddContact from './components/contacts/AddContact';
+import EditContact from './components/contacts/EditContact';
+import Header from './components/layouts/Header';
+import { Provider } from './context';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+class App extends Component {
+  render() {
+    // Here above the return we can put any kind of variables.
+    return (
+      <Provider>
+        <Router>
+          <div className="App">
+            <Header branding="Contact Manager" />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Contacts} />
+                <Route exact path="/contact/add" component={AddContact} />
+                <Route exact path="/contact/edit/:id" component={EditContact} />
+                <Route exact path="/about" component={About} />
+                <Route component={NotFound} />
+              </Switch>
+            </div>
+          </div>
+        </Router>
+      </Provider>
+    );
+  }
+}
+
+export default App;
