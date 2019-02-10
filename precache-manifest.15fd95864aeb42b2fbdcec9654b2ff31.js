508810ff157e3487ae0a58efae3f4f283d629b7f
Apoorv <apoorvm8@gmail.com>
2019-02-11 02:52:38 +0530

    Updates

---
+++ precache-manifest.15fd95864aeb42b2fbdcec9654b2ff31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5468eef6b3679363afe4",
+    "url": "/contactmanager1/static/js/runtime~main.5468eef6.js"
+  },
+  {
+    "revision": "120a1c00bd1f2eb15556",
+    "url": "/contactmanager1/static/js/main.120a1c00.chunk.js"
+  },
+  {
+    "revision": "b73de37d9a1ba8a8b1c0",
+    "url": "/contactmanager1/static/js/1.b73de37d.chunk.js"
+  },
+  {
+    "revision": "120a1c00bd1f2eb15556",
+    "url": "/contactmanager1/static/css/main.da3b6268.chunk.css"
+  },
+  {
+    "revision": "b73de37d9a1ba8a8b1c0",
+    "url": "/contactmanager1/static/css/1.d544302d.chunk.css"
+  },
+  {
+    "revision": "8cdf5e5eaa3583f8d8855eacc15fce75",
+    "url": "/contactmanager1/index.html"
+  }
+];
